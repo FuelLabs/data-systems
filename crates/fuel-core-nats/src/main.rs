6d9a9a80deef9f46145b1d466a7bc1dce18953bb
Maciej Woś <maciej.wos@gmail.com>
2024-06-05 15:35:42 +0200

    fmt

--- crates/fuel-core-nats/src/main.rs
+++ crates/fuel-core-nats/src/main.rs
@@ -5,23 +5,28 @@ use fuel_core_services::Service;
 
 #[derive(Parser)]
 pub struct Cli {
-    #[arg(long, value_name = "URL", env = "NATS_URL", default_value = "localhost:4222")]
-    nats_url: String,
-    #[command(flatten)]
-    fuel_core_config: run::Command,
+	#[arg(
+		long,
+		value_name = "URL",
+		env = "NATS_URL",
+		default_value = "localhost:4222"
+	)]
+	nats_url: String,
+	#[command(flatten)]
+	fuel_core_config: run::Command,
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    fuel_core_bin::cli::init_logging();
-    
-    let cli = Cli::parse();
-    let service = run::get_service(cli.fuel_core_config)?;
-    service.start()?;
+	fuel_core_bin::cli::init_logging();
 
-    let subscription = service.shared.block_importer.block_importer.subscribe();
+	let cli = Cli::parse();
+	let service = run::get_service(cli.fuel_core_config)?;
+	service.start()?;
 
-    fuel_core_nats::nats_publisher(subscription, cli.nats_url).await?;
+	let subscription = service.shared.block_importer.block_importer.subscribe();
 
-    Ok(())
+	fuel_core_nats::nats_publisher(subscription, cli.nats_url).await?;
+
+	Ok(())
 }
