2b03dd3e6145fcdef31b06bd4321216b3f723329
AJ <34186192+Jurshsmith@users.noreply.github.com>
2024-08-15 13:43:26 +0100

    feat(core): refactor core and publisher to integrate coherently (#124)
    * feat: refactor core and publisher to integrate coherently
    
    * refactor: centralize data parsing in Streamable
    
    * refactor: restrict bincode crate to data-parser crate
    
    * refactor: fix previous tests
    
    * refactor: include minimal publishing tests
    
    * refactor: remove old comments
    
    * refactor: resolve conflicts

--- crates/fuel-streams-core/src/nats/mod.rs
+++ crates/fuel-streams-core/src/nats/mod.rs
@@ -1,18 +1,15 @@
-mod client_opts;
-mod conn_id;
-mod conn_streams;
+/// Houses shared APIs for interacting with NATS for fuel-streams-publisher and fuel-streams crates
+/// As much as possible, the public interface/APIS should be agnostic of NATS. These can then be extended
+/// in the fuel-streams-publisher and fuel-streams crates to provide a more opinionated API towards
+/// their specific use-cases.
 mod error;
 mod nats_client;
+mod nats_client_opts;
 mod nats_namespace;
 
-pub(crate) mod stream;
-pub use fuel_streams_macros::subject::*;
 pub mod types;
 
-pub use client_opts::*;
-pub use conn_id::*;
-pub use conn_streams::*;
 pub use error::*;
 pub use nats_client::*;
+pub use nats_client_opts::*;
 pub use nats_namespace::*;
-pub use stream::*;
