9bb01f9131218742989494387718e13470959131
Pedro Nauck <pedronauck@gmail.com>
2024-08-16 07:05:00 -0300

    refactor(fuel-streams): removing streams for sdk (#142)

--- crates/fuel-streams/src/lib.rs
+++ crates/fuel-streams/src/lib.rs
@@ -1,11 +1,9 @@
 pub mod client;
 mod error;
 pub mod stream;
-mod streams;
 
 pub use error::*;
 pub use stream::*;
-pub use streams::*;
 
 pub mod prelude {
     pub use crate::{client::*, error::*, stream::*};
