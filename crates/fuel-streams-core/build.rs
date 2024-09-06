61be1f1516f9d0231ebcdc3c3e9e1b5a1c98c4b0
Pedro Nauck <pedronauck@gmail.com>
2024-09-06 20:04:09 -0300

    ci(repo): Fix publish crates

--- crates/fuel-streams-core/build.rs
+++ crates/fuel-streams-core/build.rs
@@ -3,8 +3,7 @@ use std::env;
 use dotenvy::dotenv;
 
 fn main() {
-    dotenv().expect(".env file not found");
-
+    let _ = dotenv();
     if let Ok(value) = env::var("NATS_PUBLIC_PASS") {
         println!("cargo:rustc-env=NATS_PUBLIC_PASS={}", value);
     }
