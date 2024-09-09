4eabaeac242f7b7c1c7ef5eb2e9c27b4fed232bf
Joshua Oladele <jurshsmith@gmail.com>
2024-09-09 12:44:44 +0100

    refactor(publisher): introduce  trait to simplify mocking in integration tests

--- crates/fuel-streams-publisher/src/main.rs
+++ crates/fuel-streams-publisher/src/main.rs
@@ -1,6 +1,8 @@
 //! This binary subscribes to events emitted from a Fuel client or node
 //! to publish streams that can consumed via the `fuel-streams` SDK.
 use clap::Parser;
+use fuel_streams_core::nats::{NatsClient, NatsClientOpts};
+use fuel_streams_publisher::{FuelCore, Publisher};
 
 /// CLI structure for parsing command-line arguments.
 ///
@@ -26,31 +28,35 @@ async fn main() -> anyhow::Result<()> {
 
     let cli = Cli::parse();
 
-    let fuel_core =
+    let fuel_core_service =
         fuel_core_bin::cli::run::get_service(cli.fuel_core_config).await?;
 
-    let fuel_core_subscription =
-        fuel_core.shared.block_importer.block_importer.subscribe();
-    let fuel_core_database = fuel_core.shared.database.clone();
+    fuel_core_service.start_and_await().await?;
 
-    let chain_config = fuel_core.shared.config.snapshot_reader.chain_config();
+    let fuel_core_database = fuel_core_service.shared.database.clone();
+    let blocks_subscription = fuel_core_service
+        .shared
+        .block_importer
+        .block_importer
+        .subscribe();
+
+    let chain_config = fuel_core_service
+        .shared
+        .config
+        .snapshot_reader
+        .chain_config();
     let chain_id = chain_config.consensus_parameters.chain_id();
-    let base_asset_id = chain_config.consensus_parameters.base_asset_id();
 
-    fuel_core
-        .start_and_await()
-        .await
-        .expect("Fuel core service startup failed");
+    // TODO: is this still useful?
+    let _base_asset_id = chain_config.consensus_parameters.base_asset_id();
 
-    let publisher = fuel_streams_publisher::Publisher::new(
-        &cli.nats_url,
-        chain_id,
-        *base_asset_id,
-        fuel_core_database,
-        fuel_core_subscription,
-    )
-    .await?;
-    publisher.run().await?;
+    let fuel_core =
+        FuelCore::new(fuel_core_database, blocks_subscription, chain_id).await;
+
+    let nats_client_opts = NatsClientOpts::admin_opts(&cli.nats_url);
+    let nats_client = NatsClient::connect(&nats_client_opts).await?;
+
+    Publisher::new(&nats_client, fuel_core).await.run().await?;
 
     Ok(())
 }
