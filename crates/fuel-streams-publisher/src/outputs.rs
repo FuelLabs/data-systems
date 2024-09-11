086baf90a6fd076eaa94e4fd7862e5c5581b2c5e
Maciej Woś <maciej.wos@gmail.com>
2024-09-11 08:30:47 +0200

    feat(publisher): publish outputs

--- crates/fuel-streams-publisher/src/outputs.rs
+++ crates/fuel-streams-publisher/src/outputs.rs
@@ -1,5 +1,17 @@
+<<<<<<< HEAD
 use fuel_core_types::fuel_tx::{field::Outputs, Output, UniqueIdentifier};
+||||||| parent of b1d06dd (feat(publisher): publish outputs)
+use fuel_core::schema::contract;
+use fuel_core_types::fuel_tx::{
+    field::Outputs,
+    UniqueIdentifier,
+};
+=======
+use fuel_core::schema::contract;
+use fuel_core_types::fuel_tx::{field::Outputs, Output, UniqueIdentifier};
+>>>>>>> b1d06dd (feat(publisher): publish outputs)
 use fuel_streams_core::{
+<<<<<<< HEAD
     outputs::{
         OutputsChangeSubject,
         OutputsCoinSubject,
@@ -9,8 +21,27 @@ use fuel_streams_core::{
     },
     prelude::{IntoSubject, *},
     types::{ChainId, Transaction},
+||||||| parent of b1d06dd (feat(publisher): publish outputs)
+    outputs::{
+        OutputsSubject,
+        OutputsByIdSubject,
+    },
+    types::{Bytes32, ChainId, IdentifierKind, Transaction},
+=======
+    outputs::{OutputsByIdSubject, OutputsSubject},
+    prelude::*,
+    types::{Bytes32, ChainId, IdentifierKind, Transaction},
+>>>>>>> b1d06dd (feat(publisher): publish outputs)
     Stream,
 };
+<<<<<<< HEAD
+||||||| parent of b1d06dd (feat(publisher): publish outputs)
+use fuel_streams_macros::subject::IntoSubject;
+use fuel_core_types::fuel_tx::Output;
+use fuel_streams_core::prelude::*;
+=======
+use fuel_streams_macros::subject::IntoSubject;
+>>>>>>> b1d06dd (feat(publisher): publish outputs)
 
 macro_rules! get_outputs {
     ($transaction:expr, $($variant:ident),+) => {
@@ -47,6 +78,7 @@ pub async fn publish(
         let tx_id = transaction.id(chain_id);
         let outputs = outputs_from_transaction(transaction);
         for (index, output) in outputs.iter().enumerate() {
+<<<<<<< HEAD
             let subject: Box<dyn IntoSubject> = match output {
                 Output::Coin { to, asset_id, .. } => OutputsCoinSubject::new()
                     .with_tx_id(Some(tx_id.into()))
@@ -81,8 +113,72 @@ pub async fn publish(
                 }
                 Output::Variable { to, asset_id, .. } => {
                     OutputsVariableSubject::new()
+||||||| parent of b1d06dd (feat(publisher): publish outputs)
+            let subject = match output {
+                Output::Coin { to, amount, asset_id } =>
+                    OutputsSubject::new()
                         .with_tx_id(Some(tx_id.into()))
                         .with_index(Some(index as u16))
+                        .with_output_type(Some(OutputType::Coin(
+                            CoinSubject::new()
+                                .with_from(todo!())
+                                .with_to(Some((*to).into()))
+                                .with_asset_id(Some(*asset_id))
+                        ))),
+                Output::Change { to, asset_id, .. } =>
+                    OutputsSubject::new()
+                        .with_tx_id(Some(tx_id.into()))
+                        .with_index(Some(index as u16))
+                        .with_output_type(Some(OutputType::Change(
+                            ChangeSubject::new()
+                                .with_to(Some((*to).into()))
+                                .with_asset_id(Some(*asset_id))
+                        ))),
+                Output::Contract(contract) =>
+                    OutputsSubject::new()
+                            .with_tx_id(Some(tx_id.into()))
+                            .with_index(Some(index as u16))
+                            .with_output_type(Some(OutputType::Contract(
+                                ContractSubject::new()
+                                    .with_contract_id(Some(todo!()))
+                            ))),
+                Output::ContractCreated { contract_id, .. } =>
+                    OutputsSubject::new()
+=======
+            let subject = match output {
+                Output::Coin {
+                    to,
+                    amount,
+                    asset_id,
+                } => OutputsSubject::new()
+                    .with_tx_id(Some(tx_id.into()))
+                    .with_index(Some(index as u16))
+                    .with_output_type(Some(OutputType::Coin(
+                        CoinSubject::new()
+                            .with_from(todo!())
+                            .with_to(Some((*to).into()))
+                            .with_asset_id(Some(*asset_id)),
+                    ))),
+                Output::Change { to, asset_id, .. } => OutputsSubject::new()
+                    .with_tx_id(Some(tx_id.into()))
+                    .with_index(Some(index as u16))
+                    .with_output_type(Some(OutputType::Change(
+                        ChangeSubject::new()
+                            .with_to(Some((*to).into()))
+                            .with_asset_id(Some(*asset_id)),
+                    ))),
+                Output::Contract(contract) => OutputsSubject::new()
+                    .with_tx_id(Some(tx_id.into()))
+                    .with_index(Some(index as u16))
+                    .with_output_type(Some(OutputType::Contract(
+                        ContractSubject::new().with_contract_id(Some(todo!())),
+                    ))),
+                Output::ContractCreated { contract_id, .. } => {
+                    OutputsSubject::new()
+>>>>>>> b1d06dd (feat(publisher): publish outputs)
+                        .with_tx_id(Some(tx_id.into()))
+                        .with_index(Some(index as u16))
+<<<<<<< HEAD
                         .with_to(Some((*to).into()))
                         .with_asset_id(Some(asset_id.into()))
                         .boxed()
@@ -94,6 +190,34 @@ pub async fn publish(
                         .with_contract_id(Some(*contract_id))
                         .boxed()
                 }
+||||||| parent of b1d06dd (feat(publisher): publish outputs)
+                        .with_output_type(Some(OutputType::ContractCreated(
+                            ContractCreatedSubject::new()
+                                .with_contract_id(Some(*contract_id))
+                        ))),
+                Output::Variable { to, asset_id, .. } =>
+                    OutputsSubject::new()
+                        .with_tx_id(Some(tx_id.into()))
+                            .with_index(Some(index as u16))
+                            .with_output_type(Some(OutputType::Variable(
+                                VariableSubject::new()
+                                    .with_to(Some((*to).into()))
+                                    .with_asset_id(Some(*asset_id))))),
+=======
+                        .with_output_type(Some(OutputType::ContractCreated(
+                            ContractCreatedSubject::new()
+                                .with_contract_id(Some(*contract_id)),
+                        )))
+                }
+                Output::Variable { to, asset_id, .. } => OutputsSubject::new()
+                    .with_tx_id(Some(tx_id.into()))
+                    .with_index(Some(index as u16))
+                    .with_output_type(Some(OutputType::Variable(
+                        VariableSubject::new()
+                            .with_to(Some((*to).into()))
+                            .with_asset_id(Some(*asset_id)),
+                    ))),
+>>>>>>> b1d06dd (feat(publisher): publish outputs)
             };
 
             stream.publish(&*subject, &output).await?;
--- crates/fuel-streams-publisher/src/publisher.rs
+++ crates/fuel-streams-publisher/src/publisher.rs
@@ -1,5 +1,23 @@
+<<<<<<< HEAD
 use fuel_core::database::database_description::DatabaseHeight;
 use fuel_core_types::fuel_tx::Output;
+||||||| parent of b1d06dd (feat(publisher): publish outputs)
+use fuel_core::{
+    combined_database::CombinedDatabase,
+    database::database_description::DatabaseHeight,
+};
+use fuel_core_importer::ports::ImporterDatabase;
+use fuel_core_storage::transactional::AtomicView;
+use fuel_core_types::blockchain::consensus::Sealed;
+=======
+use fuel_core::{
+    combined_database::CombinedDatabase,
+    database::database_description::DatabaseHeight,
+};
+use fuel_core_importer::ports::ImporterDatabase;
+use fuel_core_storage::transactional::AtomicView;
+use fuel_core_types::{blockchain::consensus::Sealed, fuel_tx::output::Output};
+>>>>>>> b1d06dd (feat(publisher): publish outputs)
 use fuel_streams_core::{
     blocks::BlocksSubject,
     nats::NatsClient,
@@ -14,9 +32,14 @@ use crate::{blocks, inputs, outputs, receipts, transactions, FuelCoreLike};
 pub struct Streams {
     pub transactions: Stream<Transaction>,
     pub blocks: Stream<Block>,
+<<<<<<< HEAD
     pub inputs: Stream<Input>,
     pub outputs: Stream<Output>,
     pub receipts: Stream<Receipt>,
+||||||| parent of b1d06dd (feat(publisher): publish outputs)
+=======
+    pub outputs: Stream<Output>,
+>>>>>>> b1d06dd (feat(publisher): publish outputs)
 }
 
 impl Streams {
@@ -24,21 +47,30 @@ impl Streams {
         Self {
             transactions: Stream::<Transaction>::new(nats_client).await,
             blocks: Stream::<Block>::new(nats_client).await,
+<<<<<<< HEAD
             inputs: Stream::<Input>::new(nats_client).await,
             outputs: Stream::<Output>::new(nats_client).await,
             receipts: Stream::<Receipt>::new(nats_client).await,
+||||||| parent of b1d06dd (feat(publisher): publish outputs)
+=======
+            outputs: Stream::<Output>::new(nats_client).await,
+>>>>>>> b1d06dd (feat(publisher): publish outputs)
         }
     }
 
     #[cfg(feature = "test-helpers")]
     pub async fn is_empty(&self) -> bool {
-        use fuel_streams_core::transactions::TransactionsSubject;
+        use fuel_streams_core::{
+            outputs::OutputsSubject,
+            transactions::TransactionsSubject,
+        };
 
         self.blocks.is_empty(BlocksSubject::WILDCARD).await
             && self
                 .transactions
                 .is_empty(TransactionsSubject::WILDCARD)
                 .await
+            && self.outputs.is_empty(OutputsSubject::WILDCARD).await
     }
 }
 
@@ -145,6 +177,13 @@ impl Publisher {
             &self.streams.outputs,
             self.fuel_core.chain_id(),
             transactions,
+        )
+        .await?;
+
+        outputs::publish(
+            &self.streams.outputs,
+            &self.chain_id,
+            block.transactions(),
         )
         .await?;
 
