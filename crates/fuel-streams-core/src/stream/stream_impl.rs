c5f0e27b3c4fb472056278ec39e63b6aba480a5c
0xterminator <evgeni.pirianov@fuel.sh>
2024-09-18 16:25:06 +0300

    feat(repo): Added nextest and upgraded crates (#218)

--- crates/fuel-streams-core/src/stream/stream_impl.rs
+++ crates/fuel-streams-core/src/stream/stream_impl.rs
@@ -6,7 +6,6 @@ use async_nats::{
         kv::{self, CreateErrorKind},
         stream::{self, LastRawMessageErrorKind, State},
     },
-    Message,
     RequestErrorKind,
 };
 use async_trait::async_trait;
@@ -241,7 +240,6 @@ impl<S: Streamable> Stream<S> {
 
         match message {
             Ok(message) => {
-                let message: Message = message.try_into().unwrap();
                 let payload = S::decode(message.payload.to_vec()).await;
 
                 Ok(Some(payload))
