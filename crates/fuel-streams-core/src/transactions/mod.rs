51d32d292f20a275f126b1577b7b422c9cd2d774
Joshua Oladele <jurshsmith@gmail.com>
2024-09-30 15:34:04 +0100

    refactor(core): Allow fetching inputs and outputs for fuel-streams transactions

--- crates/fuel-streams-core/src/transactions/mod.rs
+++ crates/fuel-streams-core/src/transactions/mod.rs
@@ -1,6 +1,7 @@
 pub mod subjects;
 pub mod types;
 
+use fuel_core_types::fuel_tx::{field::Outputs, Output};
 pub use subjects::*;
 use types::*;
 
@@ -13,4 +14,41 @@ impl Streamable for Transaction {
         TransactionsSubject::WILDCARD,
         TransactionsByIdSubject::WILDCARD,
     ];
+}
+
+pub trait WithTxInputs {
+    fn inputs(&self) -> &[Input];
+}
+
+pub trait WithTxOutputs {
+    fn outputs(&self) -> &Vec<Output>;
+}
+
+impl WithTxInputs for Transaction {
+    fn inputs(&self) -> &[Input] {
+        match self {
+            Transaction::Mint(_) => &[],
+            Transaction::Script(tx) => tx.inputs(),
+            Transaction::Blob(tx) => tx.inputs(),
+            Transaction::Create(tx) => tx.inputs(),
+            Transaction::Upload(tx) => tx.inputs(),
+            Transaction::Upgrade(tx) => tx.inputs(),
+        }
+    }
+}
+
+impl WithTxOutputs for Transaction {
+    fn outputs(&self) -> &Vec<Output> {
+        match self {
+            Transaction::Mint(_) => {
+                static NO_OUTPUTS: Vec<Output> = Vec::new();
+                &NO_OUTPUTS
+            }
+            Transaction::Script(tx) => tx.outputs(),
+            Transaction::Blob(tx) => tx.outputs(),
+            Transaction::Create(tx) => tx.outputs(),
+            Transaction::Upload(tx) => tx.outputs(),
+            Transaction::Upgrade(tx) => tx.outputs(),
+        }
+    }
 }
