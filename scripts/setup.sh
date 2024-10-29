0c9c45fce8efad8fd4b216807537058f98e28c3b
Pedro Nauck <pedronauck@gmail.com>
2024-10-29 19:47:27 -0300

    ci(repo): update development and CI tooling

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,6 +16,7 @@ rustup toolchain install nightly-2024-10-18 -c rustfmt
 # Install cargo global crates
 cargo install cargo-binstall
 cargo install cargo-tarpaulin
+cargo install samply --locked
 cargo install cargo-audit --locked --features=fix
 cargo install cargo-nextest --locked --features=fix
 cargo binstall --no-confirm cargo-watch knope cargo-sort typos-cli
