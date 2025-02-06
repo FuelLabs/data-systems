9cc19d588229bccf5d87fa1e451fc6a70da76682
Pedro Nauck <pedronauck@gmail.com>
2025-02-06 14:40:31 -0300

    ci(repo): Disable aarch64 linux builds

--- scripts/cross/aarch64-unknown-linux-gnu.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-dpkg --add-architecture arm64
-apt-get update
-apt-get install --assume-yes clang-8 libclang-8-dev binutils-aarch64-linux-gnu zlib1g-dev:arm64 pkg-config libudev-dev libssl-dev
