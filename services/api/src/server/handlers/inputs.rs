45326119026b14e6862c6342062ce0abf0104b2d
0xterminator <evgeni.pirianov@fuel.sh>
2025-03-07 11:43:36 +0200

    feat(repo): Added api optimizations and improvements

--- services/api/src/server/handlers/inputs.rs
+++ services/api/src/server/handlers/inputs.rs
@@ -1,24 +1,26 @@
 use actix_web::{web, HttpRequest, HttpResponse};
 use fuel_streams_domains::{
-    inputs::{queryable::InputsQuery, InputDbItem, InputType},
-    queryable::Queryable,
+    inputs::{queryable::InputsQuery, InputType},
+    queryable::{Queryable, ValidatedQuery},
 };
 use fuel_web_utils::api_key::ApiKey;
 
-use super::{Error, GetDbEntityResponse};
+use super::{Error, GetDataResponse};
 use crate::server::state::ServerState;
 
 pub async fn get_inputs(
     req: HttpRequest,
-    req_query: web::Query<InputsQuery>,
+    req_query: ValidatedQuery<InputsQuery>,
     state: web::Data<ServerState>,
     queried_input_type: Option<InputType>,
 ) -> actix_web::Result<HttpResponse> {
     let _api_key = ApiKey::from_req(&req)?;
     let mut query = req_query.into_inner();
     query.set_input_type(queried_input_type);
-    let db_records =
-        query.execute(&state.db.pool).await.map_err(Error::Sqlx)?;
-    Ok(HttpResponse::Ok()
-        .json(GetDbEntityResponse::<InputDbItem> { data: db_records }))
+    let response: GetDataResponse = query
+        .execute(&state.db.pool)
+        .await
+        .map_err(Error::Sqlx)?
+        .try_into()?;
+    Ok(HttpResponse::Ok().json(response))
 }
