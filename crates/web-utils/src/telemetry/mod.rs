36674e3bf3f4c078a64a77a62130456ffd7bab33
0xterminator <evgeni.pirianov@fuel.sh>
2025-03-19 17:45:19 +0000

    fix(sv-api): Removed system-based metrics from metrics endpoint (#446)

--- crates/web-utils/src/telemetry/mod.rs
+++ crates/web-utils/src/telemetry/mod.rs
@@ -4,7 +4,7 @@ mod runtime;
 #[allow(clippy::needless_borrows_for_generic_args)]
 mod system;
 
-use std::{sync::Arc, time::Duration};
+use std::sync::Arc;
 
 use anyhow::Context;
 use elastic_search::{
@@ -15,14 +15,11 @@ use elastic_search::{
 };
 use metrics::TelemetryMetrics;
 // TODO: Consider using tokio's Rwlock instead
-use parking_lot::RwLock;
 use runtime::Runtime;
-use system::System;
 
 #[derive(Clone)]
 pub struct Telemetry<M: TelemetryMetrics> {
     runtime: Arc<Runtime>,
-    system: Arc<RwLock<System>>,
     metrics: Option<Arc<M>>,
     elastic_search: Option<Arc<ElasticSearch>>,
 }
@@ -31,10 +28,7 @@ impl<M: TelemetryMetrics> Telemetry<M> {
     const DEDICATED_THREADS: usize = 2;
 
     pub async fn new(metrics: Option<M>) -> anyhow::Result<Arc<Self>> {
-        let runtime =
-            Runtime::new(Self::DEDICATED_THREADS, Duration::from_secs(20));
-        let system = Arc::new(RwLock::new(System::new().await));
-
+        let runtime = Runtime::new(Self::DEDICATED_THREADS);
         let elastic_search = if should_use_elasticsearch() {
             Some(Arc::new(new_elastic_search().await?))
         } else {
@@ -43,7 +37,6 @@ impl<M: TelemetryMetrics> Telemetry<M> {
 
         Ok(Arc::new(Self {
             runtime: Arc::new(runtime),
-            system,
             metrics: if should_use_metrics() {
                 metrics.map(Arc::new)
             } else {
@@ -54,8 +47,6 @@ impl<M: TelemetryMetrics> Telemetry<M> {
     }
 
     pub async fn start(&self) -> anyhow::Result<()> {
-        let system = Arc::clone(&self.system);
-
         if let Some(elastic_search) = self.elastic_search.as_ref() {
             tracing::info!(
                 "Elastic Search connection live? {:?}",
@@ -68,10 +59,6 @@ impl<M: TelemetryMetrics> Telemetry<M> {
                 .context("Error pinging elastisearch connection")?;
             tracing::info!("Elastic logger pinged successfully!");
         };
-
-        self.runtime.start(move || {
-            system.write().refresh();
-        });
 
         Ok(())
     }
@@ -138,26 +125,6 @@ impl<M: TelemetryMetrics> Telemetry<M> {
         };
 
         result.push_str(&res_custom);
-
-        // let system_metrics = match self.system.read().metrics() {
-        //     Ok(m) => {
-        //         let metrics = SystemMetricsWrapper::from(m);
-        //         let labels: Vec<(&str, &str)> = vec![];
-        //         match serde_prometheus::to_string(&metrics, None, labels) {
-        //             Ok(m) => m,
-        //             Err(err) => {
-        //                 tracing::error!("could not encode system metrics: {:?}", err);
-        //                 String::default()
-        //             }
-        //         }
-        //     }
-        //     Err(err) => {
-        //         tracing::error!("prometheus system metrics could not be stringified: {:?}", err);
-        //         String::default()
-        //     }
-        // };
-        // result.push_str(&system_metrics);
-
         result.push_str("# EOF\n");
         result
     }
