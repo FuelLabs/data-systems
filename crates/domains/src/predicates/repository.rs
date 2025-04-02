246c251206183499dba9c02a9286437388562e35
Pedro Nauck <pedronauck@gmail.com>
2025-04-01 21:06:25 -0400

    refactor(domains): Optimize predicates

--- crates/domains/src/predicates/repository.rs
+++ crates/domains/src/predicates/repository.rs
@@ -4,7 +4,7 @@ use sqlx::{Acquire, PgExecutor, Postgres};
 
 use super::{Predicate, PredicateDbItem, PredicatesQuery};
 use crate::infra::{
-    repository::{Repository, RepositoryError, RepositoryResult},
+    repository::{Repository, RepositoryResult},
     DbItem,
 };
 
@@ -21,69 +21,49 @@ impl Repository for Predicate {
         'c: 'e,
         E: PgExecutor<'c> + Acquire<'c, Database = Postgres>,
     {
+        let mut conn = executor.acquire().await?;
+        let mut tx = conn.begin().await?;
         let published_at = BlockTimestamp::now();
-        let record = sqlx::query_as::<_, PredicateDbItem>(
+        let predicate_id = sqlx::query_scalar::<_, i32>(
             r#"
-            WITH inserted_predicate AS (
-                INSERT INTO predicates (
-                    blob_id,
-                    predicate_address,
-                    created_at,
-                    published_at
-                )
-                VALUES ($1, $2, $3, $4)
-                ON CONFLICT (predicate_address) DO UPDATE
-                SET blob_id = EXCLUDED.blob_id,
-                    created_at = EXCLUDED.created_at,
-                    published_at = EXCLUDED.published_at
-                RETURNING id, blob_id, predicate_address, created_at, published_at
-            ),
-            inserted_transaction AS (
-                INSERT INTO predicate_transactions (
-                    predicate_id,
-                    cursor,
-                    subject,
-                    block_height,
-                    tx_id,
-                    tx_index,
-                    input_index,
-                    asset_id,
-                    bytecode
-                )
-                SELECT
-                    id,
-                    $5,
-                    $6,
-                    $7,
-                    $8,
-                    $9,
-                    $10,
-                    $11,
-                    $12
-                FROM inserted_predicate
-                RETURNING predicate_id
+            INSERT INTO predicates (
+                blob_id,
+                predicate_address,
+                created_at,
+                published_at
             )
-            SELECT
-                p.id,
-                p.blob_id,
-                p.predicate_address,
-                p.created_at,
-                p.published_at,
-                $5 AS cursor,
-                $6 AS subject,
-                $7 AS block_height,
-                $8 AS tx_id,
-                $9 AS tx_index,
-                $10 AS input_index,
-                $11 AS asset_id,
-                $12 AS bytecode
-            FROM inserted_predicate p
+            VALUES ($1, $2, $3, $4)
+            ON CONFLICT (predicate_address) DO UPDATE
+            SET blob_id = EXCLUDED.blob_id,
+                created_at = EXCLUDED.created_at,
+                published_at = EXCLUDED.published_at
+            RETURNING id
             "#,
         )
         .bind(&db_item.blob_id)
         .bind(&db_item.predicate_address)
         .bind(db_item.created_at)
         .bind(published_at)
+        .fetch_one(&mut *tx)
+        .await?;
+
+        sqlx::query(
+            r#"
+            INSERT INTO predicate_transactions (
+                predicate_id,
+                cursor,
+                subject,
+                block_height,
+                tx_id,
+                tx_index,
+                input_index,
+                asset_id,
+                bytecode
+            )
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+            "#,
+        )
+        .bind(predicate_id)
         .bind(db_item.cursor().to_string())
         .bind(&db_item.subject)
         .bind(db_item.block_height)
@@ -92,14 +72,11 @@ impl Repository for Predicate {
         .bind(db_item.input_index)
         .bind(&db_item.asset_id)
         .bind(&db_item.bytecode)
-        .fetch_one(executor)
-        .await
-        .map_err(|e| {
-            eprintln!("SQL error inserting predicate: {:?}", e);
-            RepositoryError::Insert(e)
-        })?;
+        .execute(&mut *tx)
+        .await?;
+        tx.commit().await?;
 
-        Ok(record)
+        Ok(db_item.to_owned())
     }
 }
 
