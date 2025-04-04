3255d7964c8192ba5486afd8d05313562651b800
Pedro Nauck <pedronauck@gmail.com>
2025-04-04 14:17:51 -0400

    feat(sv-dune): Add avro helpers

--- benches/data-parser/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use fuel_streams_domains::blocks::{Block, MockBlock};
-use rand::Rng;
-
-pub fn generate_test_block() -> Block {
-    let mut rng = rand::rng();
-    let block_height = rng.random_range(1..100000);
-    MockBlock::build(block_height.into())
-}
--- services/dune/src/lib.rs
+++ services/dune/src/lib.rs
@@ -1,1 +1,2 @@
+pub mod avro;
 pub mod s3;
