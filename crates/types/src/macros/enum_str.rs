554164fdaf9db0960e7b747d9fb4a5420387f9e7
green <xgreenx9999@gmail.com>
2025-12-03 01:18:02 +0000

    Make CI happy

--- crates/types/src/macros/enum_str.rs
+++ crates/types/src/macros/enum_str.rs
@@ -11,36 +11,6 @@ macro_rules! impl_enum_string_serialization {
             }
         }
 
-        impl<'r> sqlx::Decode<'r, sqlx::Postgres> for $name {
-            fn decode(
-                value: sqlx::postgres::PgValueRef<'r>,
-            ) -> Result<Self, Box<dyn std::error::Error + 'static + Send + Sync>> {
-                let value = <String as sqlx::Decode<sqlx::Postgres>>::decode(value)?;
-                $name::try_from(value.as_str()).map_err(|e| e.into())
-            }
-        }
-
-        impl sqlx::Type<sqlx::Postgres> for $name {
-            fn type_info() -> sqlx::postgres::PgTypeInfo {
-                sqlx::postgres::PgTypeInfo::with_name($db_name)
-            }
-        }
-
-        impl sqlx::Encode<'_, sqlx::Postgres> for $name {
-            fn encode_by_ref(
-                &self,
-                buf: &mut sqlx::postgres::PgArgumentBuffer,
-            ) -> Result<
-                sqlx::encode::IsNull,
-                Box<dyn std::error::Error + Send + Sync + 'static>,
-            > {
-                <&str as sqlx::Encode<sqlx::Postgres>>::encode_by_ref(
-                    &self.to_string().as_str(),
-                    buf,
-                )
-            }
-        }
-
         impl FromStr for $name {
             type Err = String;
             fn from_str(s: &str) -> Result<Self, Self::Err> {
