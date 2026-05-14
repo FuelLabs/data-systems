fbccd6d1c543c2790e96e7756bf246d576080908
Mike Christopher <mchristopher@users.noreply.github.com>
2026-05-14 13:25:59 -0700

    fix: log parsing

--- crates/web-utils/src/tracing.rs
+++ crates/web-utils/src/tracing.rs
@@ -6,17 +6,12 @@ use tracing_subscriber::{
 };
 
 pub fn init_tracing() -> anyhow::Result<()> {
-    let mut env_filter = EnvFilter::from_default_env()
-        .add_directive("fuel_web_utils::server=trace".parse()?);
-
-    match std::env::var("RUST_LOG") {
-        Ok(log) => {
-            env_filter = env_filter.add_directive(log.parse()?);
-        }
-        Err(_) => {
-            env_filter = env_filter.add_directive(Level::INFO.into());
-        }
+    let env_filter = if std::env::var_os("RUST_LOG").is_some() {
+        EnvFilter::try_from_default_env()?
+    } else {
+        EnvFilter::new(Level::INFO.to_string())
     }
+    .add_directive("fuel_web_utils::server=trace".parse()?);
 
     tracing_subscriber::registry()
         .with(env_filter)
